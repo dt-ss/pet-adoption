8d6bf3b62acad9371d60e980ae9c4a97d97d47af
acz-a <daniel.t@emi-is.com>
2024-06-20 16:12:50 +0300

    implemented filter api request to back

--- src/Components/Pages/MainPage.tsx
+++ src/Components/Pages/MainPage.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from "react";
 import {Grid} from "@mui/material";
-import {PetModel, PetType} from "../../Model/PetModel";
+import {PetModel} from "../../Model/PetModel";
 import PetCard from "../Core/PetCard";
 import {useSearchParams} from "react-router-dom";
-import {calculateAge, request} from "../../utils";
+import {request} from "../../utils";
 import {SavedPet} from "../../Model/SavedPet";
 import {useAtom} from "jotai/index";
 import {userAtom} from "../../Atoms";
@@ -27,14 +27,21 @@ export const MainPage = () => {
 
     // pull pets and saved pets lists from server
     useEffect(() => {
-        request('pets').then(r => r.json().then(d => {
+        const p =
+            `pets/filter?
+            ${query ? `&name=${query}` : ``}
+            ${minAge ? `&minAge=${minAge}` : ``}
+            ${maxAge ? `&maxAge=${maxAge}` : ``}
+            ${petType ? `&typeId=${petType}` : ``}`
+        console.log(p)
+        request(p).then(r => r.json().then(d => {
             setPets(d);
             user ? request(`savedPets/user/${user?.id}`).then(r => r.json().then(d => {
                 setSavedPets(d)
                 setLoading(false)
             })) : setLoading(false)
         }))
-    }, [user])
+    }, [user, query, minAge, maxAge, petType])
 
     return (loading ? <></> :
             <Grid
@@ -45,12 +52,7 @@ export const MainPage = () => {
                 justifyContent={'space-between'}
             >
                 {/* show filtered pet cards */}
-                {pets.filter(p =>
-                    p.name.toLowerCase().includes(query?.toLowerCase() || '')
-                    && (!maxAge || calculateAge(p.birthDate) <= parseInt(maxAge))
-                    && (!minAge || calculateAge(p.birthDate) >= parseInt(minAge))
-                    && (!petType || petType === PetType[p.typeId])
-                ).map(elem => (
+                {pets.map(elem => (
                     <Grid item sm={12} md={6} lg={4} xl={3} key={elem.id}>
                         <PetCard isLiked={!!savedPets.filter(e => e.petId === elem.id).length} pet={elem}/>
                     </Grid>
